f775e3c8203468714ebfe34e6cde1284958450aa
aaryad24 <internaarya24@gmail.com>
2025-06-19 21:19:13 +0530

    first commit

---
+++ client/src/pages/Favorite.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { dummyShowsData } from '../assets/assets'
+import BlurCircle from '../components/BlurCircle'
+import MovieCard from '../components/MovieCard'
+
+const Favorite = () => {
+  return dummyShowsData.length > 0 ? (
+    <div className="relative xl:px-44 overflow-hidden my-40 mb-60 px-6 md:px-16 lg:px-40 min-h-[80vh] ">
+      <BlurCircle top="150px" left="0px"></BlurCircle>
+      <BlurCircle bottom="50px" right="50px"></BlurCircle>
+      <h1 className="text-lg  font-medium my-4">
+    Your Favorite Movies
+      </h1>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div>No movies available.</div>
+  )
+}
+
+export default Favorite
